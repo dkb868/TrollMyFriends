1ea16c107fcc6c1ce244d7f1698176ecc6a8787e
Dmitri Brereton <mitrikyle@yahoo.com>
2016-02-28 09:55:22 -0800

    why u lyin?

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var session = require('express-session');
 
 var router = express.Router();
 
-router.use(session({ secret: 'keyboard cat', cookie: { maxAge: 1800000 }}));
+router.use(session({ secret: 'keyboard cat', cookie: { maxAge: 300000 }}));
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -23,6 +23,7 @@ router.get('/failure', function(req,res,next){
 });
 
 router.post('/inspire', function(req,res){
+  var url;
   var hasUsed = req.session.hasUsed;
   if (hasUsed == null){
     // Download the Node helper library from twilio.com/docs/node/install
@@ -30,8 +31,14 @@ router.post('/inspire', function(req,res){
     var accountSid = 'AC74d3f913d8417e3da6fe763e7119db38';
     var authToken = "3efc44c6e1aa92a223d77e6445bcf5ed";
     var client = require('twilio')(accountSid, authToken);
+    if(req.body.button == 1) {
+      url = "http://twimlbin.com/4b8f5d72cce9340262fb3d3169b64d52";
+    } else {
+      url = "http://twimlbin.com/d6570c0d62c865432cbbc368b27f33dd";
+    }
+
     client.calls.create({
-      url: "http://twimlbin.com/4b8f5d72cce9340262fb3d3169b64d52",
+      url:url,
       to: "+1" + req.body.number,
       from: "+12028499415"
     }, function(err, call) {
